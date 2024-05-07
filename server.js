b5f4177d7785415e4c521d8d008c6c860dc6d0ae
butterbee32 <muhirejessica@gmail.com>
2024-05-07 05:44:32 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+
+app.set("view engine", 'ejs')
+
+app.get('/', (req, res)=>{
+    res.render('index')
+
+});
+app.post('/shortUrls', (req,res) =>{
+
+})
+app.listen(process.env.PORT|| 5000);
